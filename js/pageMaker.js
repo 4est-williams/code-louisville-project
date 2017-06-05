e0fcf86b2aef7fdae9c74ccb65018f09e01fc808
Forrest Williams <dabidimas@gmail.com>
2017-06-05 17:10:38 -0400

    Each park now has unique div name

--- js/pageMaker.js
+++ js/pageMaker.js
@@ -1,6 +1,5 @@
 let message = '';
 
-
 //this is a function called the Fisher-Yates shuffle.
 //it takes all of the items in parkList and randomizes order in order to make the webpage more interesting
 function shuffle(array) {
@@ -19,11 +18,15 @@ function shuffle(array) {
   return array;
 }
 
-randomParkList = shuffle(parkList);
+const randomParkList = shuffle(parkList);
 
-for (var i = 0; i < parkList.length; i++) {
+for (var i = 0; i < randomParkList.length; i++) {
   if (randomParkList[i][activePage] != undefined) {
-    message += "<h2>" + randomParkList[i].name + "</h2>" + randomParkList[i].picture + "<p>" + randomParkList[i][activePage] + "</p>";
+    message += '<div class="'+randomParkList[i].className + '">';
+    message += "<h2>" + randomParkList[i].name + "</h2>";
+    message += randomParkList[i].picture;
+    message += "<p>" + randomParkList[i][activePage] + "</p>";
+    message += '</div>';
   } else {
   }
 }
--- js/parkList.js
+++ js/parkList.js
@@ -2,6 +2,7 @@ const parkList = [
 
   {
     name: "Cherokee Park",
+    className: 'cherokee',
     picture: '<img src="img/cherokee.jpg" alt="cherokee park">',
     description: "This Frederick Law Olmstead-designed park is located in the middle of the Highlands neighborhood in Louisville, Kentucky. This park offers a 2.4 mile loop for scenic driving, biking, walking or running. Park visitors will also be able to book a tee time at the golf course, bring a bow and arrow and use the archery range, or strap on their hiking boots and hit the trails.",
     biking: "Bikers that visit Cherokee Park will have several great routes to choose from. Road bikes can stick to the large, 2.4 mile loop and enjoy having a biking and pedestrian lane without worrying about cars, take one of the many exits to explore the Highlands neighborhood og Louisville, or take a trip to nearby Seneca Park. If you are riding a mountain bike, there are a plethora of trails that cut through the park and let you explore the beautiful inner trails. If riding a mountain bike, please don't use the trails if they are muddy.",
@@ -10,6 +11,7 @@ const parkList = [
 
   {
     name: "Carter Caves",
+    className: 'carter-caves',
     picture: '<img src="img/carter-caves.jpg" alt="Carter Caves">',
     description: "Located in far eastern Kentucky, Olive Hill is home to Carter Caves State Resort Park. Visitors of Carter Caves will be able to explore 2 different caves on their own or can pay for a guided tour of a third cave. In addition to caving, there are primitive camping sites, horse trails, and hiking trails that are dotted with small cave openings and large rock features.",
     biking: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque bibendum lacinia dolor, sed tempus ligula varius gravida. Vestibulum luctus, lorem id semper gravida, nibh ante eleifend mauris, id imperdiet dolor ipsum id nisl. Sed ipsum metus, aliquam et leo sed, pretium suscipit turpis. Nullam consectetur nisi erat, nec pharetra diam mattis vel. Etiam et nisl dapibus, sodales tellus et, congue ex. Vestibulum vitae odio enim. In ac tortor aliquam, vestibulum nulla at, pulvinar mauris. Fusce vitae nibh maximus, cursus eros ut, commodo ligula. Donec ac tellus non nulla tempus volutpat at ut urna. Suspendisse placerat ante augue. Suspendisse id ligula sit amet elit fringilla facilisis. Donec semper massa libero, id facilisis felis auctor eget. In at erat arcu. In varius varius interdum.',
@@ -19,6 +21,7 @@ const parkList = [
 
   {
     name: "Mammoth Cave National Park",
+    className: 'mammoth-cave',
     picture: '<img src="img/mammoth-cave.jpg" alt="Mammoth Cave National Park">',
     description: "Mammoth Cave National Park is in Bowling Green Kentucky. Mammoth Cave is the longest cave system in the world and consists of chambers and subterranean passageways. Sites include the Frozen Niagara section, known for waterfall-like flowstone formations, and Gothic Avenue, its ceiling covered in 19th-century visitors’ signatures. Trails take in other park features like the Green and Nolin rivers and the sinkholes of Cedar Sink.",
     biking: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque bibendum lacinia dolor, sed tempus ligula varius gravida. Vestibulum luctus, lorem id semper gravida, nibh ante eleifend mauris, id imperdiet dolor ipsum id nisl. Sed ipsum metus, aliquam et leo sed, pretium suscipit turpis. Nullam consectetur nisi erat, nec pharetra diam mattis vel. Etiam et nisl dapibus, sodales tellus et, congue ex. Vestibulum vitae odio enim. In ac tortor aliquam, vestibulum nulla at, pulvinar mauris. Fusce vitae nibh maximus, cursus eros ut, commodo ligula. Donec ac tellus non nulla tempus volutpat at ut urna. Suspendisse placerat ante augue. Suspendisse id ligula sit amet elit fringilla facilisis. Donec semper massa libero, id facilisis felis auctor eget. In at erat arcu. In varius varius interdum.',
@@ -27,6 +30,7 @@ const parkList = [
 
   {
     name: "Dale Hollow Lake",
+    className: 'dale-hollow',
     picture: '<img src="img/dale-hollow.jpg" alt ="dale hollow lake">',
     description: "Dale Hollow Lake State Resort Park is located in south-central Kentucky in the Cumberland River basin on the Obey River. The huge reservoir created by Dale Hollow Dam covers 27,700 acres in parts of Clinton and Cumberland Counties in Kentucky and Clay, Fentress, Overton, and Pickett Counties  in Tennessee. Dale Hollow Dam and Reservoir control the runoff drainage area of 935 square miles."
   }
