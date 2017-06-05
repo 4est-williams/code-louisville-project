230ac3553ec8a0d75727fdd6eaaadc6944be8cb5
Forrest Williams <dabidimas@gmail.com>
2017-06-05 16:33:21 -0400

    Wrote Pseudo for a lightbox

---
+++ js/lightbox.js
@@ -0,0 +1,6 @@
+//In this file I want to make each park clickable and have a lightbox appear with more information about the parkList
+
+//when park is clicked
+//fade rest of page
+//have park image take up the center of the screen
+//show all relevant information about the park
